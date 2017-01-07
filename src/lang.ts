5dbc26eae467acfce24d04dcae7e6a8525639aaf
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-01-07 21:52:52 +0800

    added compareString()

--- src/lang.ts
+++ src/lang.ts
@@ -31,3 +31,8 @@ export function caseFunctionLookup<A,B>(cases: Array<[A, () => B]>, target: A): 
   return caseLookup(cases, target)();
 }
 
+export function compareString(a: string, b: string): -1|0|1 {
+  if (a == b)
+    return 0;
+  return a < b ? -1 : 1;
+}
