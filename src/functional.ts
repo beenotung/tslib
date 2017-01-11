6b539f5f3d2596f2f799fe06370284b8ea0443dd
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-01-11 15:30:24 +0800

    defined type notation for implicit any functions in functional.ts

--- src/functional.ts
+++ src/functional.ts
@@ -2,34 +2,35 @@
  * Created by beenotung on 12/26/16.
  */
 
-import {curry} from "./curry";
+import {copyToArray, ObjKey, Obj} from "./lang";
 import {id} from "./curry";
-import {copyToArray} from "./lang";
+import {CurryF1, CurryF2} from "./curry.d";
+import {curry} from "./curry";
 
 export type Consumer<A> = (a: A) => void;
 export type Supplier<A> = () => A;
 
 /** take all args (ignore arity) */
-export let apply = curry(f => function () {
+export let apply = curry((f: Function) => function () {
   return id(f.apply(null, arguments));
 });
 
 /* reference : http://stackoverflow.com/questions/27996544/how-to-correctly-curry-a-function-in-javascript */
-export let prop = curry((name, o) => o[name]);
-export let length = curry(x => x.length);
-export let filter = curry((f, xs) => xs.filter(f));
-export let compose = curry((f, g, x) => f(g(x)));
-export let flip = curry((f, a, b) => f(b, a));
-export let lift = curry(a => b => a);
+export let prop = curry(<A>(name: ObjKey, o: Obj<A>): A => o[name]);
+export let length = curry(<A>(x: ArrayLike<A>): number => x.length);
+export let filter = curry(<A>(f: CurryF1<A,boolean>, xs: A[]): A[] => xs.filter(f));
+export let compose = curry(<A,B,C>(f: CurryF1<B,C>, g: CurryF1<A,B>, a: A): C => f(g(a)));
+export let flip = curry(<A,B,C>(f: CurryF2<A,B,C>) => (b: B) => (a: A): C => f(a, b));
+export let lift = curry(<A>(a: A) => (b: any) => a);
 export let compose2 = compose(compose, compose);
-export let odd = curry(x => x % 2 == 1);
-export let even = curry(x => x % 2 == 0);
+export let odd = curry((x: number) => x % 2 == 1);
+export let even = curry((x: number) => x % 2 == 0);
 export let countWhere = curry(compose2(length, filter));
 /**
  * @remark side effect
  * apply2 :: (*->a) -> (a->b) -> a -> b
  * */
-export let apply2 = curry((f, g, x) => {
+export let apply2 = curry(<A,B>(f: Function, g: CurryF1<A,B>, x: A): B => {
   f(x);
   return g(x)
 });
@@ -37,37 +38,39 @@ export let apply2 = curry((f, g, x) => {
  * @example echoF (console.log) (1) ~> console.log(1) +> return 1
  * */
 export let echoF = flip(apply2)(id);
-export let symbolFs = {};
-export let isFunction = curry(x => typeof x === 'function');
-export let isNumber = curry(x => typeof x === 'number');
-export let isString = curry(x => typeof x === 'string');
+export let symbolFs = new Map<string,CurryF2<any,any,any>>();
+export let isFunction = (x: any) => typeof x === 'function';
+export let isNumber = (x: any) => typeof x === 'number';
+export let isString = (x: any) => typeof x === 'string';
 /*
  * main :: RealWorld -> ((), RealWorld)
  * */
 /**@remark side effect */
-export let forEach = curry((f, xs) => {
+export let forEach = curry(<A>(f: Function, xs: ArrayLike<A>) => {
   /* xs is ArrayLike, might not has forEach */
   let n = xs.length;
   for (let i = 0; i < n; i++) {
     f(xs[i]);
   }
 });
-export type Maybe<A>=[A]|A[];
+export type EmptyArray<A> = ArrayLike<A>;
+export type Maybe<A> = [A] | EmptyArray<A>;
 /**
  * just :: a -> [a]
  * */
-export let just = curry(x => [x]);
+export let just = curry(<A>(x: A) => [x]);
 /**
  * none :: * -> []
  * */
-export let none = lift(x => []);
-export let eq = curry((a, b) => b === a);
-export let neq = curry((a, b) => b !== a);
-export let gt = curry((a, b) => b !== a);
+export let none = lift(<A>(x: A): any[] => []);
+export let eq = curry(<A>(a: A, b: A): boolean => b === a);
+export let neq = curry(<A>(a: A, b: A): boolean => b !== a);
+export let gt = curry((a: number|string, b: number|string): boolean => b > a);
+export let lt = curry((a: number|string, b: number|string): boolean => b < a);
 /**
  * first :: (a->Bool) -> [a] -> Maybe a
  * */
-export let first = curry((f, xs) => {
+export let first = curry(<A>(f: CurryF1<A,boolean>, xs: A[]): Maybe<A> => {
   for (let x of xs) {
     if (f(x))
       return just(x);
@@ -77,7 +80,7 @@ export let first = curry((f, xs) => {
 /**
  * any :: (a->Bool) -> [a] -> Bool
  * */
-export let any = curry((f, xs) => {
+export let any = curry(<A>(f: CurryF1<A,boolean>, xs: A[]) => {
   for (let x of xs) {
     if (f(x))
       return true;
@@ -89,32 +92,34 @@ export let any = curry((f, xs) => {
  * @remark side effect
  * define infix operator (binary function)
  * */
-export let defineSymbolF = curry((name, f) => {
-  f = curry(f);
-  return symbolFs[name] = curry((a, b) => f(a, b));
+export let defineSymbolF = curry(<A,B,C>(name: string, f: CurryF2<A,B,C>): CurryF2<A,B,C> => {
+  symbolFs.set(name, f);
+  return f;
 });
-export let add = defineSymbolF('+', (a, b) => b + a);
-export let minus = defineSymbolF('-', (a, b) => b - a);
-export let mult = defineSymbolF('*', (a, b) => b * a);
-defineSymbolF('/', (a, b) => b / a);
-export let rem = defineSymbolF('%', (a, b) => b % a);
-export let div = curry((a, b) => Math.floor(b / a));
-export let quot = curry((a, b) => (b / a) | 0);
+/* number | string atomically for a and b */
+export let add = defineSymbolF('+', (a: number, b: number) => b + a);
+/* number | string atomically for a and b */
+export let minus = defineSymbolF('-', (a: number, b: number) => b - a);
+export let mult = defineSymbolF('*', (a: number, b: number): number => b * a);
+defineSymbolF('/', (a: number, b: number): number => b / a);
+export let rem = defineSymbolF('%', (a: number, b: number): number => b % a);
+export let div = curry((a: number, b: number): number => Math.floor(b / a));
+export let quot = curry((a: number, b: number): number => (b / a) | 0);
 /** faster */
-export let quotMod = curry((a, b) => [(b / a) | 0, b % a]);
+export let quotMod = curry((a: number, b: number): [number, number] => [(b / a) | 0, b % a]);
 /* slower */
-export let divMod = curry((a, b) => {
+export let divMod = curry((a: number, b: number): [number, number] => {
   let d = Math.floor((b / a));
   return [d, b - d * a];
 });
-export let symbolF = curry(name => symbolFs[name]);
-export let composeFs = curry((fs, acc) => {
+export let symbolF = curry(<A,B,C>(name: string): CurryF2<A,B,C> => symbolFs.get(name));
+export let composeFs = curry(<A>(fs: CurryF1<A,A>[], acc: A) => {
   for (let i = fs.length - 1; i >= 0; i--) {
     acc = fs[i](acc);
   }
   return acc;
 });
-export let chainFs = curry((fs, acc) => {
+export let chainFs = curry(<A>(fs: CurryF1<A,A>[], acc: A) => {
   for (let f of fs) {
     acc = f(acc);
   }
@@ -125,7 +130,7 @@ export let chainFs = curry((fs, acc) => {
  * f :: unary function <A,B>
  * args :: ArrayLike<A>
  * */
-export let doAll = curry((f: Function, args) => {
+export let doAll = curry(<A>(f: Consumer<A>, args: A[]) => {
   for (let arg of args) {
     f(arg);
   }
