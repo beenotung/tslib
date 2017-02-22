141cac985c4c1f413118872a40558d213c0b4058
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-02-22 12:30:36 +0800

    added comment in Random.nextInt()

--- src/random.ts
+++ src/random.ts
@@ -2,6 +2,7 @@
  * Created by beenotung on 2/1/17.
  */
 export namespace Random {
+  /** @return number : lower <= value < upper */
   export function nextInt(upper: number, lower: number = 0): number {
     return Math.floor(Math.random() * (upper - lower) + lower);
   }
