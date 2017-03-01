1eaf1c5c79575f41afa56102b42ea943848f1976
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-03-01 17:59:36 +0800

    added type stub for ng2-chart

--- src/ng-chart.ts
+++ src/ng-chart.ts
@@ -28,3 +28,27 @@ export function mkChartColor(r: number, g: number, b: number, a: number = 0): Ng
     pointHoverBorderColor: `rgba(${rgb},0.8)`
   };
 }
+export class LineChartData {
+  data: number[];
+  label: string;
+}
+export class NgChartParams {
+  lineChartData: LineChartData[];
+  lineChartLabels: String[];
+  lineChartOptions?: {
+    responsive?: boolean
+  };
+  lineChartColors?: NgChartColor[];
+  lineChartLegend?: boolean;
+  lineChartType: LineChartType;
+
+  /* set default config */
+  constructor(data: LineChartData[], labels: String[], type: LineChartType) {
+    this.lineChartData = data;
+    this.lineChartLabels = labels;
+    this.lineChartOptions = {responsive: true};
+    this.lineChartLegend = true;
+    this.lineChartType = type;
+  }
+}
+export type LineChartType = 'line' | 'pie' | string;
