5f04d5c7047fc0856ccc4d3b14a76eadd4d6134c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-03-03 14:24:44 +0800

    updated comment in random.ts

--- src/random.ts
+++ src/random.ts
@@ -1,13 +1,10 @@
-/**
- * Created by beenotung on 2/1/17.
- */
 export namespace Random {
   /** @return number : lower <= value < upper */
   export function nextInt(upper: number = Number.MAX_SAFE_INTEGER, lower: number = 0): number {
     return Math.floor(Math.random() * (upper - lower) + lower);
   }
 
-  /** @return number : -1 .. 1 */
+  /** @return real number : -1 .. 1 */
   export function nextNP1() {
     return Math.random() * 2 - 1;
   }
