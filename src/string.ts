d63a46ee5c96c6789a8c005a628197ca9fef240f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-03-03 12:30:17 +0800

    added func strToCapWords()

--- src/string.ts
+++ src/string.ts
@@ -1,8 +1,28 @@
-/**
- * Created by beenotung on 3/2/17.
- */
+import {forI} from "./lang";
 export function str_contains(pattern: string, target: string, ignore_case = false): boolean {
   if (ignore_case)
     return str_contains(pattern.toLowerCase(), target.toLowerCase());
   return target.indexOf(pattern) != -1;
+}
+
+/**
+ * example : 'change the words' ~> 'Change The Words'
+ * */
+export function strToCapWords(s: string): string {
+  let res = '';
+  let lastSpace = true;
+  forI(i => {
+    if (s[i] == ' ') {
+      lastSpace = true;
+      res += ' ';
+    } else {
+      if (lastSpace) {
+        res += s[i].toUpperCase();
+        lastSpace = false;
+      } else {
+        res += s[i];
+      }
+    }
+  }, s.length);
+  return res;
 }
