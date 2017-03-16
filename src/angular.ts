af2efa3ac7ba3f027b0dd044196fa1566f34b893
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-03-16 16:28:56 +0800

    added CommonControlValueAccessor for [(ngModel)] usage

--- src/angular.ts
+++ src/angular.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController, NavParams} from "ionic-angular";
 import {Type} from "./lang";
+import {ControlValueAccessor} from "@angular/forms";
 
 /**
  * for the sake of type check on the param
@@ -23,4 +24,47 @@ export declare class TypedNavParams<A> extends NavParams {
 }
 export function typedNavParams<A>(navParams: NavParams): TypedNavParams<A> {
   return navParams;
+}
+
+/**
+ * to usage [(ngModel)] directly
+ * */
+export class CommonControlValueAccessor<T> implements ControlValueAccessor {
+  private innerValue: T;
+
+  get value(): T {
+    return this.innerValue
+  }
+
+  set value(v: T) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  writeValue(obj: T): void {
+    if (obj !== this.innerValue) {
+      this.innerValue = obj;
+    }
+  }
+
+  onChangeCallback(event: any): void {
+  }
+
+  onTouchedCallback(): void {
+  }
+
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+    fn(this.innerValue);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
