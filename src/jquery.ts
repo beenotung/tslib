a1882bc61258c7637df2e14aac5efaaeadde7db5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-05-31 18:08:57 +0800

    added commented out jquery ajax alternative

--- src/jquery.ts
+++ src/jquery.ts
@@ -4,5 +4,7 @@ export function $ToPromise<A>(p: JQueryPromise<A>): Promise<A> {
   const defer = createDefer<A, any>();
   p.done(defer.resolve)
     .fail(defer.reject);
+  // p.done((data,textStatus,jqXHR)=>defer.resolve({data:data,textStatus:textStatus,jqXHR:jqXHR}))
+  //   .fail((req,status,error)=>defer.reject({req:req,status:status,error:error}));
   return defer.promise;
 }
