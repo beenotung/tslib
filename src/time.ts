c2da3d43d7238e261a6bc712ad731e639d330d07
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-03 01:48:22 +0800

    added session in time.ts

--- src/time.ts
+++ src/time.ts
@@ -3,3 +3,24 @@ export const SECOND = MILLISECOND * 1000;
 export const MINUTE = SECOND * 60;
 export const HOUR = MINUTE * 60;
 export const DAY = HOUR * 24;
+export const WEEK = DAY * 7;
+
+export namespace session {
+  let intervals = {};
+
+  export function now(interval = 1000): number {
+    const now = Date.now();
+    const last = intervals[interval];
+    if (last) {
+      if (now - last < interval) {
+        return last;
+      } else {
+        intervals[interval] = now;
+        return now;
+      }
+    } else {
+      intervals[interval] = now;
+      return now;
+    }
+  }
+}
