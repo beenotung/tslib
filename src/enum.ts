21e076a2addd6de39eb01eb5016464883463e390
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-02 11:11:07 +0800

    added enum_is_in_range()

--- src/enum.ts
+++ src/enum.ts
@@ -1,4 +1,5 @@
 import {range} from "./array";
+import {isDefined} from "./lang";
 
 export type Enum = { [index: number]: string } & { [key: string]: number } | Object;
 
@@ -12,6 +13,7 @@ export function enum_i2s<E extends Enum>(e: E, i: number): keyof E {
   }
   return res;
 }
+
 export function enum_s2i<E extends Enum>(e: E, s: keyof E): number {
   const res = e[s];
   if (res === void 0) {
@@ -27,6 +29,7 @@ export function enum_next_i<E extends Enum>(e: E, i: number): number {
   }
   return res;
 }
+
 export function enum_next_s<E extends Enum>(e: E, s: string): keyof E {
   const res = e[e[s] + 1];
   if (res === void 0) {
@@ -38,6 +41,7 @@ export function enum_next_s<E extends Enum>(e: E, s: string): keyof E {
 export function enum_keys<E extends Enum>(e: E): string[] {
   return Object.keys(e).filter(x => typeof x !== 'number');
 }
+
 export function enum_values<E extends Enum>(e: E): number[] {
   return range(0, Object.keys(e).length / 2 - 1);
 }
@@ -45,19 +49,27 @@ export function enum_values<E extends Enum>(e: E): number[] {
 export function enum_last_i<E extends Enum>(e: E): number {
   return Object.keys(e).length / 2 - 1;
 }
+
 export function enum_last_s<E extends Enum>(e: E): string {
   return e[enum_last_i(e)];
 }
+
 export function enum_is_last_i<E extends Enum>(e: E, i: number): boolean {
   return i === enum_last_i(e);
 }
+
 export function enum_is_last_s<E extends Enum>(e: E, s: string): boolean {
   return s === enum_last_s(e);
 }
+
 export function enum_is_last<E extends Enum>(e: E, v: keyof E): boolean {
   if (typeof v === 'number') {
     return enum_is_last_i(e, <any>v);
   } else {
     return enum_is_last_s(e, v);
   }
+}
+
+export function enum_is_in_range<E extends Enum>(e: E, v: any): boolean {
+  return isDefined(e[v]);
 }
