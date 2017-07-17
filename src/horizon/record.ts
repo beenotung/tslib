e22faeee6687fc6f104caf2fe7a7205d8769a85e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-17 14:23:12 +0800

    changed to use full feature of rxjs Observable

--- src/horizon/record.ts
+++ src/horizon/record.ts
@@ -1,4 +1,4 @@
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 import {CreatedObject, DataType, Horizon, OldRecord, TableObject} from 'typestub-horizon-client';
 
 /**
