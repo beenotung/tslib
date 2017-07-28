6f52d9a3dbaa94e9213a47284426d4341568b1fe
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-28 11:21:11 +0800

    fine tune monad impl

--- src/functional/monad.ts
+++ src/functional/monad.ts
@@ -1,15 +1,23 @@
 /** reference : https://github.com/douglascrockford/monad/blob/master/monad.js*/
-export interface Unit {
-  method();
+import {toArray} from "../array";
 
-  lift_value();
+export interface Monad {
+  bind(f: Function, args?: ArrayLike<any>): Monad;
 }
 
-export function MONAD(modifier?: Function) {
+export interface Unit {
+  (value): Monad;
+
+  method(name: PropertyKey, f: Function);
+
+  lift_value(name: PropertyKey, f: Function);
+}
+
+export function createUnit(modifier?: Function): Unit {
   const prototype = Object.create(null);
   prototype.is_monad = true;
 
-  function unit(value) {
+  function unit(value): Monad {
     const monad = Object.create(prototype);
     monad.bind = function (f: Function, args: any[] = []) {
       return f.apply(void 0, value, ...args);
@@ -20,19 +28,32 @@ export function MONAD(modifier?: Function) {
     return monad;
   }
 
-  unit["method"] = function (name: PropertyKey, f: Function) {
-    prototype[name] = f;
-    return unit;
-  };
-
-  unit["lift_value"] = function (name: PropertyKey, f: Function) {
-    prototype[name] = function () {
-      return this.bind(f, arguments);
-    };
-    return unit;
-  };
-
-  return unit;
+  const res = Object.assign(
+    function unit(value): Monad {
+      const monad = Object.assign(Object.create(prototype), {
+        bind: function bind(f: Function, args?: ArrayLike<any>): Monad {
+          toArray(args);
+          return f.apply(void 0, value, ...args);
+        }
+      });
+      if (typeof modifier === "function") {
+        value = modifier(monad, value);
+      }
+      return monad;
+    }
+    , {
+      method: function method(name: PropertyKey, f: Function) {
+        prototype[name] = f;
+        return res;
+      }
+      , lift_value: function lift_value(name: PropertyKey, f: Function) {
+        prototype[name] = function () {
+          return (<Monad>this).bind(f, arguments);
+        };
+        return res;
+      }
+    });
+  return res;
 }
 
 
