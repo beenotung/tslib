9effe87b2f8d4d6d3ce5972f8c38673b1774949d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-08-02 12:42:11 +0800

    updated test script in package.json; added image resize function;

---
+++ image.ts
@@ -0,0 +1,23 @@
+import {createDefer} from "./src/async";
+
+/**
+ * reference : https://stackoverflow.com/questions/20958078/resize-a-base-64-image-in-javascript-without-using-canvas
+ * */
+export function imageToBase64(img: HTMLImageElement, width: number, height: number): string {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(img, 0, 0, width, height);
+  return canvas.toDataURL();
+}
+
+export async function resizeBase64Image(data: string, targetWidth: number, targetHeight: number): Promise<string> {
+  const defer = createDefer<string, any>();
+  const image = new Image;
+  image.onload = () => {
+    defer.resolve(imageToBase64(image, targetWidth, targetHeight));
+  };
+  image.src = data;
+  return defer.promise;
+}
