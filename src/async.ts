09ad99ab4b5a91d87d67132f4204cea3a8e640e7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-11-08 09:56:36 +0800

    fixing fetch argument typescript error

--- src/async.ts
+++ src/async.ts
@@ -116,12 +116,13 @@ export function clearAllTimer() {
 
 export function fetch_no_cache(url: string, method = "GET"): Promise<Response> {
   const req: Request | string = typeof Request === "function" ? new Request(url) : url;
+  const headers = new Headers();
+  headers.append("pragma", "no-cache");
+  headers.append("cache-control", "no-cache");
   const init = {
     method
-    , headers: {
-      "pragma": "no-cache"
-      , "cache-control": "no-cache"
-    }
+    , cache: "no-cache" as RequestCache
+    , headers
   };
   return fetch(req, init);
 }
