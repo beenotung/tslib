a6fe5c19805ab11aacafae973ae1d766d214350c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-11-27 13:19:59 +0800

    added getWindownProp()

--- src/window.ts
+++ src/window.ts
@@ -1,5 +1,9 @@
-import {setProp} from "./functional";
+import {prop, setProp} from "./functional";
 
 export function setWindowProp(key: string, value: any) {
   setProp(value, key, window);
+}
+
+export function getWindownProp(key: string) {
+  prop(key, window);
 }
