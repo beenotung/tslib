5b0897782094d98eeb4e64915c6190d2105ce841
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-02-10 04:17:12 +0800

    updated readStreamByLine from data-loader

--- src/logic.ts
+++ src/logic.ts
@@ -2,4 +2,5 @@ export function xor(a: boolean | any, b: boolean | any): boolean {
   return !!((a ? 1 : 0) ^ (b ? 1 : 0));
 }
 
+/** inclusive */
 export const between = (a, b, c) => a <= b && b <= c;
