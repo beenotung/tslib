d4b84f3872a2c1218b6f4e4f9c34916078f36e32
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-02-12 15:15:31 +0800

    added reduceMap in map.ts

--- src/map.ts
+++ src/map.ts
@@ -1,13 +1,21 @@
 export function mapMap<K, V, A>(map: Map<K, V>, mapper: (v: V, k: K) => A) {
   const res = new Map<A, V[]>();
-  map.forEach((v, k) => {
-    const a = mapper(v, k);
+  map.forEach((value, key) => {
+    const a = mapper(value, key);
     const vs = res.get(a);
     if (vs) {
-      vs.push(v);
+      vs.push(value);
     } else {
-      res.set(a, [v]);
+      res.set(a, [value]);
     }
   });
   return res;
+}
+
+export function reduceMap<K, V, A>(map: Map<K, V>, mapper: (acc: A, v: V, k: K) => A, initial: A): A {
+  let acc = initial;
+  map.forEach((value, key) => {
+    acc = mapper(acc, value, key);
+  });
+  return acc;
 }
