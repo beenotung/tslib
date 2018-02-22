a3196e8ef2fa55b517009dd32786a1b2206e8fe2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-02-22 15:11:57 +0800

    import html.ts and retry.ts from data-loader project

---
+++ src/html.ts
@@ -0,0 +1,55 @@
+export function toString(o: any): string {
+  switch (typeof o) {
+    case "string":
+      return o;
+  }
+  if (o instanceof Date) {
+    return (o as Date).toString();
+  }
+  if (o instanceof Set) {
+    return toString(Array.from(o as Set<any>));
+  }
+  return JSON.stringify(o, undefined, 2);
+}
+
+const escape_space = "&nbsp;";
+
+export function displayJSON(o: any, mode: "raw" | "table" = "table"): string {
+  if (mode === "raw") {
+    return `<pre>${toString(o)}</pre>`;
+  }
+  /* mode == 'table' */
+  switch (typeof o) {
+    case "object":
+      if (Array.isArray(o)) {
+        return "<ol>" + (o as any[]).map(x => "<li>" + displayJSON(x, mode) + "</li>").join("") + "</ol>";
+      }
+      if (o instanceof Set) {
+        o = Array.from(o);
+        return "<ul>" + (o as any[]).map(x => "<li>" + displayJSON(x, mode) + "</li>").join("") + "</ul>";
+      }
+      if (o instanceof Date) {
+        return o.toString();
+      }
+      break;
+    case "string":
+      const s = o as string;
+      return s
+        .split("\r").join("")
+        .split("\n").join("<br>")
+        .split("  ").join(escape_space.repeat(2))
+        .split("\t").join(escape_space.repeat(4))
+        ;
+    default:
+      return `<pre>${JSON.stringify(o)}</pre>`;
+  }
+  /* being object */
+  const rows = Object.keys(o)
+    .map(k => {
+      const v = o[k];
+      return `<tr><td>${displayJSON(k, mode)}</td><td>${displayJSON(v, mode)}</td></tr>`;
+    })
+    .join("")
+  ;
+  return `<table><tbody>${rows}</tbody></table>`;
+}
---
+++ src/retry.ts
@@ -0,0 +1,8 @@
+import {later} from "./async";
+
+export const tryN = <A>(n: number, f: () => Promise<A>, interval = 0, e?): Promise<A> => n < 0
+  ? Promise.reject(e)
+  : f().catch(e => {
+    const h = () => tryN(n - 1, f, interval, e);
+    return interval ? later(interval).then(h) : h();
+  });
