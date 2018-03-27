be2f344fa3a2e8c38008b4c11820abcb87a2dcf0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-03-27 10:14:41 +0800

    fixed init bug of KVMap

---
+++ test/kv-map-test.ts
@@ -0,0 +1,9 @@
+import {KVMap} from "../src/kv-map";
+
+const map = new KVMap();
+map.set(1, 2);
+map.set(3, 4);
+console.log('keys:', map.keys());
+console.log('values:', map.values());
+console.log('2?', map.get(1));
+console.log('1?', map.getKey(2));
