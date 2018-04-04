8b5a9116d4a836267861939d1b6ccade3bf5af1a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-04-04 10:59:53 +0800

    added tokenizer helper functions

---
+++ src/token.ts
@@ -0,0 +1,12 @@
+/**
+ * be aware of s[s.length] --> undefined
+ * */
+
+const isBetween = (l, m, r) => l <= m && m <= r;
+export type CharChecker = (c: string) => boolean;
+export const isDigit: CharChecker = c => isBetween("0", c, "9");
+export const isLowerCase: CharChecker = c => isBetween("a", c, "z");
+export const isUpperCase: CharChecker = c => isBetween("A", c, "Z");
+export const isAlphabet: CharChecker = c => isLowerCase(c) || isUpperCase(c);
+export const isWhiteSpace: CharChecker = c => c == " " || c == "\t";
+export const isLineBreak: CharChecker = c => c == "\n" || c == "\r";
