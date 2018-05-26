2f1797c8186076efccea84aa9849c5408787cd8f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-05-27 03:30:11 +0800

    added promisified writeFile from fs

--- src/fs.ts
+++ src/fs.ts
@@ -4,14 +4,28 @@ import * as fs from "fs";
  * resolve :: Buffer
  * reject :: NodeJS.ErrnoException
  * */
-export function readFile(filename): Promise<Buffer> {
+export function readFile(filename: string): Promise<Buffer> ;
+export function readFile(filename: string,
+                         options: { encoding?: string | null; flag?: string; } | string | undefined | null): Promise<string | Buffer> ;
+export function readFile(filename: string,
+                         options?: { encoding?: string | null; flag?: string; } | string | undefined | null): Promise<string | Buffer> {
   return new Promise((resolve, reject) => {
-    fs.readFile(filename, (err, data) => {
+    fs.readFile(filename, options, (err, data) => {
       if (err) {
         reject(err);
       } else {
         resolve(data);
       }
+    });
+  });
+}
+
+export function writeFile(filename: string,
+                          data,
+                          options: { encoding?: string | null; mode?: number | string; flag?: string; } | string | undefined | null): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, options, err => {
+      err ? reject(err) : resolve();
     });
   });
 }
