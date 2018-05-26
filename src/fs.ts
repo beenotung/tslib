d3f13f9209c79e80d52ee66c690458688202808d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-05-27 03:32:17 +0800

    fixed for tslint

--- src/fs.ts
+++ src/fs.ts
@@ -1,14 +1,16 @@
 import * as fs from "fs";
 
+export type readOptions = { encoding?: string | null; flag?: string; } | string | undefined | null;
+
 /**
  * resolve :: Buffer
  * reject :: NodeJS.ErrnoException
  * */
 export function readFile(filename: string): Promise<Buffer> ;
 export function readFile(filename: string,
-                         options: { encoding?: string | null; flag?: string; } | string | undefined | null): Promise<string | Buffer> ;
+                         options: readOptions): Promise<string | Buffer> ;
 export function readFile(filename: string,
-                         options?: { encoding?: string | null; flag?: string; } | string | undefined | null): Promise<string | Buffer> {
+                         options?: readOptions): Promise<string | Buffer> {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, options, (err, data) => {
       if (err) {
@@ -20,9 +22,12 @@ export function readFile(filename: string,
   });
 }
 
+export type writeOptions =
+  { encoding?: string | null; mode?: number | string; flag?: string; } | string | undefined | null;
+
 export function writeFile(filename: string,
                           data,
-                          options: { encoding?: string | null; mode?: number | string; flag?: string; } | string | undefined | null): Promise<any> {
+                          options: writeOptions): Promise<any> {
   return new Promise((resolve, reject) => {
     fs.writeFile(filename, data, options, err => {
       err ? reject(err) : resolve();
