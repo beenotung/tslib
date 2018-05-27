804ed39ff2324d197943d8bd639ceae691a1f48a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-05-27 11:35:35 +0800

    added writeStream.write for fs

--- src/fs.ts
+++ src/fs.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import {WriteStream} from "fs";
 
 export type readOptions = { encoding?: string | null; flag?: string; } | string | undefined | null;
 
@@ -33,4 +34,16 @@ export function writeFile(filename: string,
       err ? reject(err) : resolve();
     });
   });
+}
+
+export namespace writeStream {
+  export function write(stream: WriteStream, chunk, encoding?: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      if (encoding) {
+        const res = stream.write(chunk, encoding, err => err ? reject(err) : resolve(res));
+      } else {
+        const res = stream.write(chunk, err => err ? reject(err) : resolve(res));
+      }
+    });
+  }
 }
