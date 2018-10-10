fe081393af19fe32cb7886c48ad20ef4d1fd551f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-10-10 23:51:02 +0800

    removing index from the package
    since it reference to both browser and node env, cannot use the root index directly

--- mk-package.js
+++ mk-package.js
@@ -1,6 +1,4 @@
 let p = require('./package.json');
 p.scripts={};
 p.devDependencies={};
-p.main='index.js';
-p.types='index.d.ts';
-console.log(JSON.stringify(p,undefined,2))
+console.log(JSON.stringify(p,undefined,2));
