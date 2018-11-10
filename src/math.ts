956d7117c20110978d319ada8c85293c9cb85950
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-11-10 20:14:49 +0800

    added gcd and lcm in math.ts

--- src/math.ts
+++ src/math.ts
@@ -18,3 +18,27 @@ export let math: { phi: number } = {
     throw new Error('unexpected assignment');
   },
 };
+
+export function gcd (a: number, b: number): number {
+  a = Math.abs(a);
+  b = Math.abs(b);
+  if (b > a) {
+    const t = a;
+    a = b;
+    b = t;
+  }
+  for (; ;) {
+    if (b === 0) {
+      return a;
+    }
+    a %= b;
+    if (a === 0) {
+      return b;
+    }
+    b %= a;
+  }
+}
+
+export function lcm (a: number, b: number): number {
+  return !a || !b ? 0 : Math.abs(a * b) / gcd(a, b);
+}
