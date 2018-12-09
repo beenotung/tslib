d2137727c1aa586beb509fca76397acb9df6c1fa
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-12-09 22:50:44 +0800

    added enum string function test cases

--- test/enum-test.ts
+++ test/enum-test.ts
@@ -1,8 +1,25 @@
-import {enum_keys, enum_values} from "../src/enum";
+import { Enum, enum_keys, enum_only_string, enum_set_string, enum_values } from '../src/enum';
 
 enum Color {
   red, green, blue
 }
 
+console.log('keys', Object.keys(Color));
+
 console.log('enum_values', enum_values(Color));
 console.log('enum_keys', enum_keys(Color));
+
+console.log('enum', Color);
+
+enum_set_string(Color);
+console.log('set string', Color);
+
+enum_only_string(Color);
+console.log('only string', Color);
+
+function testEnum(e: Enum) {
+  console.log('passed as enum', e);
+  console.log('keys', enum_keys(e));
+}
+
+testEnum(Color);
