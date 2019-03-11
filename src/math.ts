dd101fad6e11386b36395fe66c702e54cb09624c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-03-11 17:12:32 +0800

    refactor: numToAB -> numberToFraction (number.ts -> math.ts) add: reduceFraction

--- src/math.ts
+++ src/math.ts
@@ -42,3 +42,38 @@ export function gcd(a: number, b: number): number {
 export function lcm(a: number, b: number): number {
   return !a || !b ? 0 : Math.abs(a * b) / gcd(a, b);
 }
+
+/**
+ * input: x
+ * output: [a, b], where x = a/b
+ * */
+export function numberToFraction(x: number, error = 1e-12): [number, number] {
+  if (x === Math.round(x)) {
+    return [x, 1];
+  }
+  if (x < 0) {
+    const [a, b] = numberToFraction(-x, error);
+    return [-a, b];
+  }
+  let a = 1;
+  let b = 1;
+  for (;;) {
+    const y = a / b;
+    // process.stdout.write(`\r diff=${y - x}, x=${x}, ${a}/${b}`);
+    if (x === y || Math.abs(y - x) < error) {
+      return [a, b];
+    }
+    if (y > x) {
+      b++;
+    } else {
+      a++;
+    }
+  }
+}
+
+export function reduceFraction([a, b]: [number, number]): [number, number] {
+  const c = gcd(a, b);
+  a /= c;
+  b /= c;
+  return b < 0 ? [-a, -b] : [a, b];
+}
--- src/number.ts
+++ src/number.ts
@@ -22,31 +22,3 @@ export function compare_number(a: number, b: number): CompareResult {
     ? CompareResult.Smaller
     : CompareResult.Larger;
 }
-
-/**
- * input: x
- * output: [a, b], where x = a/b
- * */
-export function numToAB(x: number, error = 1e-12): [number, number] {
-  if (x === Math.round(x)) {
-    return [x, 1];
-  }
-  if (x < 0) {
-    const [a, b] = numToAB(-x);
-    return [-a, b];
-  }
-  let a = 1;
-  let b = 1;
-  for (;;) {
-    const y = a / b;
-    // process.stdout.write(`\r diff=${y - x}, x=${x}, ${a}/${b}`);
-    if (x === y || Math.abs(y - x) < error) {
-      return [a, b];
-    }
-    if (y > x) {
-      b++;
-    } else {
-      a++;
-    }
-  }
-}
