a7cab0c816d4c3cc90dc048d5cd5b32416460866
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-04-24 15:19:59 +0800

    removed return type on catchMain

--- src/node.ts
+++ src/node.ts
@@ -1,5 +1,5 @@
-export function catchMain(p: Promise<any>) {
-  return p.catch(e => {
+export function catchMain(p: Promise<any>): void {
+  p.catch(e => {
     console.error(e);
     process.exit(1);
   });
