b86fd95058abc1696454b44fd7b782df85666c57
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-05-14 09:25:13 +0800

    added thenF() and error listener to then() and thenF()

--- src/result.ts
+++ src/result.ts
@@ -2,9 +2,31 @@ export type Result<T> = T | Promise<T>;
 
 const PromiseString = Promise.resolve().toString();
 
-export function then<T, R>(x: Result<T>, f: (x: T) => Result<R>): Result<R> {
+export function then<T, R>(
+  x: Result<T>,
+  f: (x: T) => Result<R>,
+  onError?: (e: any) => void,
+): Result<R> {
   if (x && typeof x === 'object' && x.toString() === PromiseString) {
-    return (x as Promise<T>).then(f);
+    const res = (x as Promise<T>).then(f);
+    if (onError) {
+      res.catch(onError);
+    }
+    return res;
   }
   return f(x as T);
+}
+
+export function thenF<T, R>(
+  f: () => Result<T>,
+  q: (x: T) => Result<R>,
+  onError?: (e: any) => void,
+): Result<R> {
+  try {
+    const x = f();
+    return then(x, q, onError);
+  } catch (e) {
+    onError(e);
+    return Promise.reject(e);
+  }
 }
