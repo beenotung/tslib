95cb4c4fd8f8d2902c545a3d3dd7d4dfb9c806e0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-07-15 20:39:36 +0800

    added batchProcess() in task/data-processor.ts

---
+++ src/task/data-processor.ts
@@ -0,0 +1,80 @@
+import { then } from '../result';
+
+/**
+ * auto adjust concurrent size to optimize concurrency
+ *
+ * data loading can be in arbitrary order,
+ * data processing must be in order
+ *
+ * this impl is faster than TaskPool for large number of keys
+ *
+ * maxConcurrent: manually adjust to avoid out of memory
+ * */
+export async function batchProcess<K, D>(args: {
+  keys: K[];
+  loader: (key: K) => Promise<D>;
+  processor: (datum: D, key: K) => void | Promise<void>;
+  maxConcurrent?: number;
+}): Promise<void> {
+  const { keys, loader, processor } = args;
+  const maxConcurrent = args.maxConcurrent || Number.MAX_SAFE_INTEGER;
+  if (maxConcurrent < 1) {
+    throw new Error('require at least 1 maxConcurrent');
+  }
+  /*
+  // this is bad, because it will hold all the data in memory before consuming them
+  // also, it waste the processor resources by waiting for all IO to finish before processing
+  return Promise.all(keys.map(key => loader(key))).then(data => data.forEach(datum => processor(datum)));
+  */
+  return new Promise<void>((resolve, reject) => {
+    let nLoading = 0;
+    const fail = e => {
+      nLoading = Number.MAX_SAFE_INTEGER;
+      reject(e);
+    };
+    let nextLoadIndex = 0;
+    let nextProcessIndex = 0;
+
+    const loadedDataBuffer = new Map<number, { key: K; datum: D }>();
+    const onLoad = (datum: D, key: K): void => {
+      then(
+        processor(datum, key),
+        () => {
+          // finished processing
+          nextProcessIndex++;
+          if (nextProcessIndex >= keys.length) {
+            resolve();
+          } else if (loadedDataBuffer.has(nextProcessIndex)) {
+            const { key, datum } = loadedDataBuffer.get(nextProcessIndex);
+            loadedDataBuffer.delete(nextProcessIndex);
+            onLoad(datum, key);
+          }
+        },
+        fail,
+      );
+    };
+    const load = () => {
+      if (nextLoadIndex >= keys.length) {
+        return;
+      }
+      const loadingIndex = nextLoadIndex;
+      nextLoadIndex++;
+      const key = keys[loadingIndex];
+      loader(key)
+        .then(datum => {
+          if (loadingIndex === nextProcessIndex) {
+            // can process immediately
+            onLoad(datum, key);
+          } else {
+            // cannot process yet, store to buffer
+            loadedDataBuffer.set(loadingIndex, { key, datum });
+          }
+          load();
+        })
+        .catch(fail);
+    };
+    for (let i = 0; i < maxConcurrent; i++) {
+      load();
+    }
+  });
+}
---
+++ test/data-processor.ts
@@ -0,0 +1,39 @@
+import { later } from '../src/async/wait';
+import { format_time_duration } from '../src/format';
+import { catchMain } from '../src/node';
+import { batchProcess } from '../src/task/data-processor';
+
+let clearLine = () => process.stdout.write('\r' + ' '.repeat(32) + '\r');
+
+let n = 1000 * 1000;
+let delay = 1000;
+let keys = new Array(n).fill(0).map((x, i) => i);
+let startTime = Date.now();
+let last = -1;
+catchMain(
+  batchProcess({
+    maxConcurrent: 1000 * 100,
+    keys,
+    loader: key => later(delay * Math.random()).then(() => {
+      return ({
+        key,
+        value: key.toString().repeat(10000),
+      });
+    }),
+    processor: ({ key, value }) => {
+      if (key < last) {
+        clearLine();
+        console.error({ last, key });
+        throw new Error('not processing in order');
+      }
+      last = key;
+      // process.stdout.write('\rprocessing: ' + key);
+    },
+  }).then(() => {
+    clearLine();
+    let endTime = Date.now();
+    let usedTime = endTime - startTime;
+    console.log('used: ', format_time_duration(usedTime));
+    console.log('average TPS: ' + (n / (usedTime / 1000)));
+  }));
+
