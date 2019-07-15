a6f23ecbc823debece64db0063f2a1b79e196917
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-07-15 19:05:59 +0800

    updated task-pool-test.ts for last refactor on task-pool

--- test/task-pool-test.ts
+++ test/task-pool-test.ts
@@ -1,9 +1,9 @@
-import {TaskPool} from "./runner/task-pool";
-import {later} from "../src/async/wait";
+import { later } from '../src/async/wait';
+import { TaskPool } from '../src/task/task-pool';
 
 const pool = new TaskPool(2);
 for (let i = 0; i < 10; i++) {
-  pool.run(() => new Promise(async (resolve, reject) => {
+  pool.runTask(() => new Promise(async (resolve, reject) => {
     console.log('begin', i);
     await later(1000);
     console.log(' end ', i);
