b9971c14f39a7c638541763bfcc459379463e691
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-10-24 23:57:12 +0800

    removed dep on axios on download-file.ts
    will not auto upgrade to https when given http url

--- src/download-file.ts
+++ src/download-file.ts
@@ -2,22 +2,31 @@
  * For Node
  * */
 
-import axios from 'axios';
 import * as fs from 'fs';
+import { IncomingMessage } from 'http';
+import * as http from 'http';
+import * as https from 'https';
 
 /**
  * reference: https://stackoverflow.com/a/51624229/3156509
  * */
 export const download_file = (url: string, file_path: string) =>
-  axios({
-    url,
-    responseType: 'stream',
-  }).then(
-    response =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(file_path))
-          .on('finish', () => resolve())
-          .on('error', (e: any) => reject(e));
-      }),
-  );
+  new Promise((resolve, reject) => {
+    const callback = (response: IncomingMessage) =>
+      response
+        .pipe(fs.createWriteStream(file_path))
+        .on('error', (err: any) => {
+          reject(err);
+        })
+        .on('finish', () => {
+          resolve();
+        });
+    if (url.startsWith('http://')) {
+      return http.get(url, callback);
+    } else if (url.startsWith('https://')) {
+      return https.get(url, callback);
+    } else {
+      console.error('unknown protocol on url:', url);
+      throw new Error('unknown protocol');
+    }
+  });
