f2f449155ad41ecc192068e6ee8ee0ac4c18d84d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-06 15:28:52 +0800

    added constant for number unit

--- src/constant/all.ts
+++ src/constant/all.ts
@@ -1,10 +1,12 @@
 import { facebook } from './facebook';
 import { ionic } from './ionic';
 import { notification } from './notification';
+import { number } from './number';
 
 export const constant = Object.assign(
   {},
   ionic.constant,
   notification.constant,
   facebook.constant,
+  number.constant,
 );
---
+++ src/constant/number.ts
@@ -0,0 +1,10 @@
+export namespace number {
+  export namespace constant {
+    export const ten = 10;
+    export const hundred = 100;
+    export const thousand = 1000;
+    export const million = thousand * thousand;
+    export const billion = million * thousand;
+    export const trillion = billion * thousand;
+  }
+}
