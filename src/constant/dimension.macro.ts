739b989ad1f2655ae13b4e7a55e4c49e3322c98d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-11 21:38:57 +0800

    added dimension of image and mobile screen as constant

---
+++ src/constant/dimension.macro.ts
@@ -0,0 +1,33 @@
+let sizes = `
+UHD 3840x2160
+QHD 2560x1440
+FHD 1920x1080
+ HD 1280x720
+VGA  640x480
+
+iPhone5  320x568
+iPhone6  375x667
+GalaxyS5 360x640
+`;
+ const code =  sizes
+    .split('\n')
+    .map(line=>line.split(' ')
+      .filter(s=>s))
+    .filter(xs=>xs.length>0)
+    .map(xs=>{
+      let[name,dimension]=xs
+      let [w,h]=dimension.split('x')
+      return `export const ${name}: Dimension = { width: ${w}, height: ${h} };`
+    })
+    .join('\n')
+      ;
+(function() {
+  return `
+export type Dimension = {
+  width: number;
+  height: number;
+};
+
+${code}
+`.trim() + '\n';
+})();
---
+++ src/constant/dimension.ts
@@ -0,0 +1,13 @@
+export type Dimension = {
+  width: number;
+  height: number;
+};
+
+export const UHD: Dimension = { width: 3840, height: 2160 };
+export const QHD: Dimension = { width: 2560, height: 1440 };
+export const FHD: Dimension = { width: 1920, height: 1080 };
+export const HD: Dimension = { width: 1280, height: 720 };
+export const VGA: Dimension = { width: 640, height: 480 };
+export const iPhone5: Dimension = { width: 320, height: 568 };
+export const iPhone6: Dimension = { width: 375, height: 667 };
+export const GalaxyS5: Dimension = { width: 360, height: 640 };
