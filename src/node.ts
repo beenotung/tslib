dd5640d1817d9f978915271063f8713528bc9f84
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-15 12:30:34 +0800

    added timer for profiling in node.js

--- src/node.ts
+++ src/node.ts
@@ -4,3 +4,75 @@ export function catchMain(p: Promise<any>): void {
     process.exit(1);
   });
 }
+
+export function eraseChars(writeStream: NodeJS.WriteStream, n: number) {
+  if (n < 1) {
+    return;
+  }
+  writeStream.write(' '.repeat(n));
+  writeStream.moveCursor(-n, 0);
+}
+
+export type StartTimerOptions =
+  | string
+  | {
+      name: string;
+      writeStream?: NodeJS.WriteStream;
+    };
+
+const defaultWriteStream = () => process.stdout;
+
+export function startTimer(options: StartTimerOptions) {
+  let name: string;
+  let writeStream: NodeJS.WriteStream;
+  if (typeof options === 'string') {
+    name = options;
+    writeStream = defaultWriteStream();
+  } else {
+    name = options.name;
+    writeStream = options.writeStream || defaultWriteStream();
+  }
+  let msgLen = 0;
+  const print = (msg: string) => {
+    writeStream.moveCursor(-msgLen, 0);
+    writeStream.write(msg);
+    const newMsgLen = msg.length;
+    eraseChars(writeStream, msgLen - newMsgLen);
+    msgLen = newMsgLen;
+  };
+  const start = () => {
+    writeStream.write(new Date().toLocaleString() + ': ' + name);
+    print(' ...');
+    // tslint:disable-next-line no-console
+    console.time(name);
+  };
+  const end = () => {
+    print('');
+    writeStream.moveCursor(-name.length, 0);
+    // tslint:disable-next-line no-console
+    console.timeEnd(name);
+  };
+  let total = 0;
+  let tick = 0;
+  const progress = (msg: string) => {
+    print(msg);
+  };
+  start();
+  return {
+    end,
+    next(newName: string) {
+      end();
+      name = newName;
+      start();
+    },
+    progress,
+    setProgress(totalTick: number, initialTick = 0) {
+      total = totalTick;
+      tick = initialTick;
+    },
+    tick() {
+      tick++;
+      progress(` (${tick}/${total})`);
+    },
+  };
+}
---
+++ test/node-test.ts
@@ -0,0 +1,19 @@
+import { later } from '../src/async/wait';
+import { startTimer } from '../src/node';
+
+async function main() {
+  let timer = startTimer('first task');
+  await later(100);
+  timer.next('second task');
+  timer.setProgress(2);
+  await later(1500);
+  timer.tick();
+  await later(1000)
+  timer.tick()
+  await later(500)
+  timer.next('last task');
+  await later(200);
+  timer.end();
+}
+
+main();
