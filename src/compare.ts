041cb85d5843520aea0e5b5922080cd606fdae35
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-23 10:22:11 +0800

    do not require the second generic type in compare_by_keys()

--- src/compare.ts
+++ src/compare.ts
@@ -1,11 +1,9 @@
+export type Comparable = number | string;
+
 /**
  * inclusive
  * */
-export function isBetween<A extends number | string>(
-  a: A,
-  b: A,
-  c: A,
-): boolean {
+export function isBetween<A extends Comparable>(a: A, b: A, c: A): boolean {
   return a <= b && b <= c;
 }
 
@@ -15,7 +13,7 @@ export enum CompareResult {
   Larger = 1,
 }
 
-export function compare<A extends number | string>(a: A, b: A): CompareResult {
+export function compare<A extends Comparable>(a: A, b: A): CompareResult {
   return a < b
     ? CompareResult.Smaller
     : a > b
@@ -23,21 +21,21 @@ export function compare<A extends number | string>(a: A, b: A): CompareResult {
     : CompareResult.Equal;
 }
 
-export type SortKey<K> = {
-  key: K;
+export type SortKey<T extends object> = {
+  key: keyof T;
   order: 'asc' | 'desc';
 };
 
-export function compare_by_keys<T extends object, K extends keyof T>(
-  _keys: Array<K | SortKey<K>>,
+export function compare_by_keys<T extends object>(
+  _keys: Array<SortKey<T> | keyof T>,
 ) {
-  const keys: Array<SortKey<K>> = _keys.map(key => {
+  const keys: Array<SortKey<T>> = _keys.map(key => {
     if (typeof key === 'object') {
       return key;
     }
     return { key, order: 'asc' };
   });
-  return <T extends Record<K, number | string>>(a: T, b: T): CompareResult => {
+  return <R extends Record<keyof T, Comparable>>(a: R, b: R): CompareResult => {
     for (const { key, order } of keys) {
       const cmp = compare(a[key], b[key]);
       if (cmp !== 0) {
