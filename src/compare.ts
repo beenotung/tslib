b0349215093ca6e988059df3e155c08c90f8f499
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-23 08:28:54 +0800

    enhanced compare_by_keys to allow descending compare

--- src/compare.ts
+++ src/compare.ts
@@ -23,17 +23,27 @@ export function compare<A extends number | string>(a: A, b: A): CompareResult {
     : CompareResult.Equal;
 }
 
+export type SortKey<K> = {
+  key: K;
+  order: 'asc' | 'desc';
+};
+
 export function compare_by_keys<T extends object, K extends keyof T>(
-  keys: K[],
+  _keys: Array<K | SortKey<K>>,
 ) {
+  const keys: Array<SortKey<K>> = _keys.map(key => {
+    if (typeof key === 'object') {
+      return key;
+    }
+    return { key, order: 'asc' };
+  });
   return <T extends Record<K, number | string>>(a: T, b: T): CompareResult => {
-    let cmp = 0;
-    for (const key of keys) {
-      cmp = compare(a[key], b[key]);
+    for (const { key, order } of keys) {
+      const cmp = compare(a[key], b[key]);
       if (cmp !== 0) {
-        break;
+        return order === 'desc' ? -cmp : cmp;
       }
     }
-    return cmp;
+    return 0;
   };
 }
