aad89b8fe1e40521eda711c96b1ff13c0cc32f9f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-23 08:19:21 +0800

    added compare_by_keys for chained compare

--- src/compare.ts
+++ src/compare.ts
@@ -22,3 +22,18 @@ export function compare<A extends number | string>(a: A, b: A): CompareResult {
     ? CompareResult.Larger
     : CompareResult.Equal;
 }
+
+export function compare_by_keys<T extends object, K extends keyof T>(
+  keys: K[],
+) {
+  return <T extends Record<K, number | string>>(a: T, b: T): CompareResult => {
+    let cmp = 0;
+    for (const key of keys) {
+      cmp = compare(a[key], b[key]);
+      if (cmp !== 0) {
+        break;
+      }
+    }
+    return cmp;
+  };
+}
---
+++ test/compare-test.ts
@@ -0,0 +1,31 @@
+import test from 'tape';
+import { compare, compare_by_keys } from '../src/compare';
+
+let numbers = [1, 10, 3, 2];
+let sorted = [1, 2, 3, 10];
+test('sort numeric number', t => {
+  t.deepEquals(numbers.slice().sort(compare), sorted);
+  t.end();
+});
+
+test('sort object array by key', t => {
+  t.deepEquals(
+    numbers.map(x => ({ x })).sort(compare_by_keys(['x'])),
+    sorted.map(x => ({ x })),
+  );
+  t.deepEquals(
+    [
+      { a: 10, b: 10 },
+      { a: 1, b: 1 },
+      { a: 10, b: 1 },
+      { a: 1, b: 10 },
+    ].sort(compare_by_keys(['a', 'b'])),
+    [
+      { a: 1, b: 1 },
+      { a: 1, b: 10 },
+      { a: 10, b: 1 },
+      { a: 10, b: 10 },
+    ],
+  );
+  t.end();
+});
