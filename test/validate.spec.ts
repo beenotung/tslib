393e786e695614d4d84bcc0c5e347d6ae0b11301
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-02-21 06:29:25 +0800

    add test case for to_full_hk_mobile_phone()

--- test/validate.spec.ts
+++ test/validate.spec.ts
@@ -1,5 +1,6 @@
-import { is_email } from '../src/validate'
+import { is_email, to_full_hk_mobile_phone } from '../src/validate'
 import { t } from './tape-adaptor'
+import { expect } from 'chai'
 
 describe('validate.ts spec', () => {
   it('should validate email', () => {
@@ -9,5 +10,16 @@ describe('validate.ts spec', () => {
     t.equal(false, is_email('user'))
     t.equal(false, is_email(''))
     t.end()
+  })
+  describe('validate hk mobile phone', () => {
+    it('preserve "+852" prefix if present in input', () => {
+      expect(to_full_hk_mobile_phone('+85298765432')).to.equals('+85298765432')
+    })
+    it('add "+852" prefix if absent in input', () => {
+      expect(to_full_hk_mobile_phone('98765432')).to.equals('+85298765432')
+    })
+    it('add "+" prefix if absent in input', () => {
+      expect(to_full_hk_mobile_phone('85298765432')).to.equals('+85298765432')
+    })
   })
 })
