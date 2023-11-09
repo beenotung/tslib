6dcfcca99a986edadf27cf96300b413fdbb38c98
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-09 13:32:28 +0800

    chore: remove parcel from devDep

--- test/web/index.ts
+++ test/web/index.ts
@@ -1,8 +1,4 @@
-// this file should be loaded by `npx parcel serve test/web/index.html`
-import { Stream } from 'stream'
 import { setWindowProp } from '../../src/window'
 import * as lib from '../../src/dom'
 
 setWindowProp('lib', lib)
-
-setWindowProp('Stream', Stream)
