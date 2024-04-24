7b3ca5836bbec1cb0a1790b38b97e8a4bf2e9340
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-25 04:20:52 +0800

    feat: add isIP() in url.ts

--- src/url.ts
+++ src/url.ts
@@ -32,3 +32,64 @@ export function parseURLSearchParams(
   })
   return params
 }
+
+// IPv6 format reference: https://www.ietf.org/rfc/rfc2732.txt
+export function isIP(url: string): boolean {
+  // e.g. "http://", "https://", "ftp://"
+  let index = url.indexOf('://')
+  if (index != -1) {
+    url = url.substring(index + '://'.length)
+  }
+
+  // e.g. "example.net/about-us"
+  index = url.indexOf('/')
+  if (index != -1) {
+    url = url.substring(0, index)
+  }
+
+  // e.g. "[2001:0000:130F:0000:0000:09C0:876A:130B]:8100"
+  let match = url.match(/\[(.*?)\]/)
+  if (match) {
+    url = match[1]
+  }
+
+  // e.g. "::FFFF:129.144.52.38"
+  if (url.toUpperCase().startsWith('::FFFF:')) {
+    url = url.substring(7)
+  }
+  // e.g. "::127.0.0.1"
+  else if (url.startsWith('::')) {
+    url = url.substring(2)
+  }
+
+  // e.g. "127.0.0.1:8100"
+  let parts = url.split(':')[0].split('.')
+  if (
+    parts.length == 4 &&
+    Number.isInteger(+parts[0]) &&
+    Number.isInteger(+parts[1]) &&
+    Number.isInteger(+parts[2]) &&
+    Number.isInteger(+parts[3])
+  ) {
+    return true
+  }
+
+  // e.g. "2001:0000:130F:0000:0000:09C0:876A:130B"
+  // e.g. "1080::8:800:200C:417A"
+  parts = url.split(':')
+  if (parts.every(part => part == '' || isHex(part))) {
+    return true
+  }
+
+  return false
+}
+
+function isHex(part: string): boolean {
+  for (let char of part) {
+    if ('0' <= char && char <= '9') continue
+    if ('a' <= char && char <= 'f') continue
+    if ('A' <= char && char <= 'F') continue
+    return false
+  }
+  return true
+}
--- test/url.spec.ts
+++ test/url.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { parseURLSearchParams } from '../src/url'
+import { isIP, parseURLSearchParams } from '../src/url'
 
 describe('url.ts TestSuit', () => {
   describe('parseURLSearchParams()', () => {
@@ -25,6 +25,36 @@ describe('url.ts TestSuit', () => {
       let url = 'a=b&c="d"'
       let output = parseURLSearchParams(url, { parse: 'json' })
       expect(output).to.deep.equals({ a: 'b', c: 'd' })
+    })
+  })
+
+  describe('isIP()', () => {
+    it('should remove protocol prefix', () => {
+      expect(isIP('http://127.0.0.1')).to.be.true
+      expect(isIP('https://127.0.0.1')).to.be.true
+      expect(isIP('ftp://127.0.0.1')).to.be.true
+      expect(isIP('http://example.net')).to.be.false
+    })
+    it('should remove tailing pathname', () => {
+      expect(isIP('127.0.0.1/')).to.be.true
+      expect(isIP('127.0.0.1/about-us')).to.be.true
+      expect(isIP('example.net/')).to.be.false
+      expect(isIP('example.net/about-us')).to.be.false
+    })
+    it('should remove port number', () => {
+      expect(isIP('127.0.0.1:8100')).to.be.true
+      expect(isIP('example.net:8100')).to.be.false
+    })
+    it('should detect IPv4', () => {
+      expect(isIP('127.0.0.1')).to.be.true
+    })
+    it('should detect IPv6', () => {
+      expect(isIP('2001:0000:130F:0000:0000:09C0:876A:130B')).to.be.true
+      expect(isIP('::127.0.0.1')).to.be.true
+      expect(isIP('[2001:0000:130F:0000:0000:09C0:876A:130B]:8100')).to.be.true
+    })
+    it('should not confuse with domain having numbers', () => {
+      expect(isIP('163.com')).to.be.false
     })
   })
 })
