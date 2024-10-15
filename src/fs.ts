e34a99963581c00b40fdb7352f000a1876feed51
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-10-15 17:04:00 +0800

    breaking: use built-in fs/promises instead of util.promisify for fs operations

--- src/fs.ts
+++ src/fs.ts
@@ -1,33 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import * as util from 'util'
 import { Result } from './result'
-
-export const copyFile: typeof fs.copyFile.__promisify__ = util.promisify(
-  fs.copyFile,
-)
-
-/**
- * resolve :: Buffer
- * reject :: NodeJS.ErrnoException
- * */
-export const readFile: typeof fs.readFile.__promisify__ = util.promisify(
-  fs.readFile,
-)
-export const writeFile: typeof fs.writeFile.__promisify__ = util.promisify(
-  fs.writeFile,
-)
-export const readdir: typeof fs.readdir.__promisify__ = util.promisify(
-  fs.readdir,
-)
-export const unlink: typeof fs.unlink.__promisify__ = util.promisify(fs.unlink)
-export const rename: typeof fs.rename.__promisify__ = util.promisify(fs.rename)
-/** Does not dereference symbolic links. */
-export const lstat: typeof fs.lstat.__promisify__ = util.promisify(fs.lstat)
-/** Does dereference symbolic links */
-export const stat: typeof fs.stat.__promisify__ = util.promisify(fs.stat)
-export const mkdir: typeof fs.mkdir.__promisify__ = util.promisify(fs.mkdir)
-export const exists: typeof fs.exists.__promisify__ = util.promisify(fs.exists)
+import { lstat, readFile, readdir, stat, writeFile } from 'fs/promises'
 
 function isNoFileError(e: any): true | Promise<any> {
   if (e.code === 'ENOENT') {
