8f295af462ce5c537a31afb51acec7e646aa649f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-02-18 22:14:33 +0800

    feat: support getting progress in download_file()

--- src/download-file.ts
+++ src/download-file.ts
@@ -6,15 +6,51 @@ import { createWriteStream } from 'fs'
 import { Readable } from 'stream'
 import { finished } from 'stream/promises'
 
-/**
- * reference: https://stackoverflow.com/a/51302466
- * */
-export async function download_file(url: string, file_path: string) {
+export type Options = {
+  url: string
+  file: string
+  onProgress?: (progress: Progress) => void
+}
+
+type Progress = {
+  total?: number
+  current: number
+  chunk: number
+}
+
+export async function download_file(
+  url: string,
+  file: string,
+  onProgress?: (progress: Progress) => void,
+): Promise<void>
+export async function download_file(options: Options): Promise<void>
+export async function download_file(
+  arg1: string | Options,
+  arg2?: string,
+  arg3?: (progress: Progress) => void,
+): Promise<void> {
+  const url = typeof arg1 === 'string' ? arg1 : arg1.url
+  const file = typeof arg1 === 'string' ? arg2! : arg1.file
+  const onProgress = typeof arg1 === 'string' ? arg3 : arg1.onProgress
   const res = await fetch(url)
   if (!(200 <= res.status && res.status <= 299)) {
     throw res
   }
-  await finished(
-    Readable.fromWeb(res.body as any).pipe(createWriteStream(file_path)),
-  )
+  if (!onProgress) {
+    // reference: https://stackoverflow.com/a/51302466
+    await finished(
+      Readable.fromWeb(res.body as any).pipe(createWriteStream(file)),
+    )
+    return
+  }
+
+  const total = Number(res.headers.get('content-length')) || undefined
+  let current = 0
+  const stream = createWriteStream(file)
+  for await (const chunk of res.body as any) {
+    stream.write(chunk)
+    current += chunk.length
+    onProgress({ total, current, chunk: chunk.length })
+  }
+  stream.close()
 }
