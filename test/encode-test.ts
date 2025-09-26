53681ce0c1e3cb066d08c86c704882da3fdb387b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-09-26 18:53:00 +0800

    test: add more test cases for urlEncode()

--- test/encode-test.ts
+++ test/encode-test.ts
@@ -1,15 +1,25 @@
 import { urlEncode } from '../src/encode'
 
-function test(s: any) {
-  s = JSON.stringify(s)
+function test(o: object) {
+  let native = new URLSearchParams(o as any).toString()
+  let custom = urlEncode(o)
   console.log({
-    before: s,
-    after: urlEncode(s),
+    json: JSON.stringify(o),
+    native,
+    custom,
+    re_native: new URLSearchParams(native),
+    re_custom: new URLSearchParams(custom),
   })
 }
 
 test({
   name: 'tester',
+})
+test({
+  name: 'tester 123',
+})
+test({
+  name: 'tester+123',
 })
 test({
   name: 'tester=123',
@@ -18,5 +28,20 @@ test({
   name: 'tester&123',
 })
 test({
+  name: 'tester%123',
+})
+test({
   name: '1+2*3/4^5\\6 7&8=9',
+})
+test({
+  items: [1, 2, 3],
+})
+test({
+  items: ['1', '2', '3'],
+})
+test({
+  user: { id: 1, username: 'tester' },
+})
+test({
+  name: null,
 })
