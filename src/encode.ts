916bd0b28f7746025b56a04bffdefee48e26f7d4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-09-26 18:09:39 +0800

    chore: simplify urlEncode() to not depends on lang.ts and it's dependencies

--- src/encode.ts
+++ src/encode.ts
@@ -1,6 +1,4 @@
-import { forI, Obj, objToArray } from './lang'
-
-export function urlEncode(o: Obj<string | number>): string {
+export function urlEncode(o: object): string {
   function escape(x: any): string {
     const type = typeof x
     if (type === 'number') {
@@ -8,7 +6,7 @@ export function urlEncode(o: Obj<string | number>): string {
     }
     if (type === 'string') {
       let res = ''
-      forI(i => {
+      for (let i = 0; i < x.length; i++) {
         const c: string = x[i]
         if (
           ('A' <= c && c <= 'Z') ||
@@ -19,13 +17,13 @@ export function urlEncode(o: Obj<string | number>): string {
         } else {
           res += '%' + c.charCodeAt(0).toString(16)
         }
-      }, x.length)
+      }
       return res
     }
     return escape(JSON.stringify(x))
   }
 
-  return objToArray(o)
-    .map(vk => escape(vk[1]) + '=' + escape(vk[0]))
+  return Object.entries(o)
+    .map(([k, v]) => escape(k) + '=' + escape(v))
     .join('&')
 }
