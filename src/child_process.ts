ebd10ee15d41025a2a7c152e40a2d9ea4567ebb3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-14 00:44:50 +0800

    feat: throw error with stdout and stderr - instead of just reject with code (no stack trace) patch: do not exit process on failure - allow custom error handling

--- src/child_process.ts
+++ src/child_process.ts
@@ -7,6 +7,17 @@ import * as util from 'util'
 
 export const exec = util.promisify(_exec)
 
+export class SpawnError extends Error {
+  public processError?: any
+  public code?: number
+  constructor(
+    public stdout: string,
+    public stderr: string,
+  ) {
+    super()
+  }
+}
+
 export function spawn(options: {
   cmd: string
   args?: string[]
@@ -21,29 +32,44 @@ export function spawn(options: {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     cmd = args.shift()!
   }
+  let stdout = ''
+  let stderr = ''
   return new Promise<number | null>((resolve, reject) => {
     const child = _spawn(cmd, args, options.options)
     child.stdout.setEncoding('utf8')
-    child.stdout.on(
-      'data',
-      options.on_stdout || (data => process.stdout.write(data.toString())),
-    )
-    child.stderr.setEncoding('utf8')
-    child.stderr.on(
-      'data',
-      options.on_stderr || (data => process.stderr.write(data.toString())),
-    )
-    child.on('error', err => {
-      if (options.on_error) {
-        options.on_error(err)
+    child.stdout.on('data', data => {
+      stdout += data.toString()
+      if (options.on_stdout) {
+        options.on_stdout(data)
       } else {
-        console.error(err)
-        process.exit(1)
+        process.stdout.write(data.toString())
       }
+    })
+    child.stderr.setEncoding('utf8')
+    child.stderr.on('data', data => {
+      stderr += data.toString()
+      if (options.on_stderr) {
+        options.on_stderr(data)
+      } else {
+        process.stderr.write(data.toString())
+      }
+    })
+    child.on('error', err => {
+      const error = new SpawnError(stdout, stderr)
+      error.processError = err
+      if (options.on_error) {
+        options.on_error(error)
+      }
+      reject(error)
     })
     child.on('close', code => {
       if (code) {
-        reject(code)
+        const error = new SpawnError(stdout, stderr)
+        error.code = code
+        if (options.on_error) {
+          options.on_error(error)
+        }
+        reject(error)
       } else {
         resolve(code)
       }
